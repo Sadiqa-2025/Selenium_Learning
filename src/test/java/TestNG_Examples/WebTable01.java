8cb8400a02f93f2089a5242f72cae4aa42e8fc69
Sadiqa-2025 <sadiqanishat059@gmail.com>
2025-10-29 21:29:46 +0530

    28 oct

--- src/test/java/TestNG_Examples/WebTable01.java
+++ src/test/java/TestNG_Examples/WebTable01.java
@@ -8,6 +8,7 @@ import org.testng.annotations.Test;
 
 /*
 for the contact "Helen Bennett" , i need company name and contact name.
+for the country 'canada' get the company name
  */
 public class WebTable01 {
 
@@ -19,6 +20,9 @@ public class WebTable01 {
         System.out.println("The company name is "+ companyName.getText());
         WebElement countryName = driver.findElement(By.xpath("//table[@id='customers']//td[text() ='Helen Bennett']/following-sibling::td"));
         System.out.println("The country name is "+ countryName.getText());
+        //for the country 'canada' get the company name
+        WebElement canada = driver.findElement(By.xpath("//table[@id='customers']//td[text() ='Canada']/preceding-sibling::td"));
+        System.out.println("the company name for Canada is "+ canada.getText());
         driver.quit();
     }
 }
