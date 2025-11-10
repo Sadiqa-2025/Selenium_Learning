249e841053fb81cd8cf4a73e8408700c6a735102
Sadiqa-2025 <sadiqanishat059@gmail.com>
2025-11-10 18:56:19 +0530

    10 Nov

--- src/test/java/TestNG_Examples/ReadFromExcel.java
+++ src/test/java/TestNG_Examples/ReadFromExcel.java
@@ -1,13 +1,18 @@
 package TestNG_Examples;
 
+import org.apache.poi.xssf.usermodel.XSSFCell;
+import org.apache.poi.xssf.usermodel.XSSFRow;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.testng.annotations.Test;
 
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.IOException;
 
 public class ReadFromExcel {
     @Test
-    public void readExcel() throws FileNotFoundException {
+    public void readExcel() throws IOException {
         // workbook > sheet > row > cell > XSSF
         /*
         To get path of excel
@@ -17,8 +22,24 @@ public class ReadFromExcel {
         row
         cell
          */
-        String path = "D:\\Selenium Learning\\Selenium_Learning\\src\\main\\resources";
+        String path = "D:\\Selenium Learning\\Selenium_Learning\\src\\main\\resources\\DayCountry.xlsx";
         FileInputStream file = new FileInputStream(path);
+        XSSFWorkbook workbook = new XSSFWorkbook(file);
+        XSSFSheet sheet = workbook.getSheet("Sheet1");
+        int noOfRows= sheet.getLastRowNum();
+        int noOfCells = sheet.getRow(1).getLastCellNum();
+        System.out.println(noOfRows);
+        System.out.println(noOfCells);
+        for(int row=0; row<=noOfRows; row++){
+            XSSFRow currentRow = sheet.getRow(row);
+            for(int cell=0; cell<noOfCells; cell++){
+                XSSFCell currentCell = currentRow.getCell(cell);
+                System.out.print(currentCell + "\t");
+            }
+            System.out.println();
+        }
+        file.close();
+        workbook.close();
 
     }
 }
