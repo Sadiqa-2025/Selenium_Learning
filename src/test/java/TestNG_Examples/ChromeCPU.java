3ed6760d222712ac591f3ad9f13904f45cc8bb0e
Sadiqa-2025 <sadiqanishat059@gmail.com>
2025-11-10 18:39:44 +0530

    10 Nov

--- src/test/java/TestNG_Examples/ChromeCPU.java
+++ src/test/java/TestNG_Examples/ChromeCPU.java
@@ -1,7 +1,9 @@
 package TestNG_Examples;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
 
@@ -15,10 +17,22 @@ Compare it with value in the yellow label."
  */
 public class ChromeCPU {
     @Test
-    public void CPUloadForChrome(){
+    public void CPUloadForChrome() throws InterruptedException {
         WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
         driver.get("https://practice.expandtesting.com/dynamic-table#google_vignette");
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
-        driver.findElement(By.xpath("//tr/td[text()='Chrome']"));
+        JavascriptExecutor js = (JavascriptExecutor) driver;
+        js.executeScript("window.scrollTo(0,600);");
+        WebElement cpuLoad= driver.findElement(By.xpath("//tbody/tr/td[text()='Chrome']/following-sibling::td[position()=count(//thead/tr/th[text()='CPU'])+2]"));
+        String loadValue = cpuLoad.getText();
+        String yellowValue = driver.findElement(By.xpath("//p[@id='chrome-cpu']")).getText();
+        if(yellowValue.contains(loadValue)){
+            System.out.println("The value is equal");
+        }
+        else System.out.println("The value is not equal");
+        Thread.sleep(5000);
+        driver.quit();
     }
+
 }
